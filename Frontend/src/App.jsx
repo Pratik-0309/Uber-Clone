96dc3b9099afcda0ab2db11e32461ff67ba59fbf
PratikParate <paratepratik10@gmail.com>
2026-03-26 13:17:09 +0530

    add routing with react-router-dom and update App component structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import UserLogin from "./pages/UserLogin.jsx";
+import UserSignup from "./pages/UserSignup.jsx";
+import CaptainLogin from "./pages/CaptainLogin.jsx";
+import CaptainSignup from "./pages/CaptainSignup.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
